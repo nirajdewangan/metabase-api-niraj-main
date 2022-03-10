77102d2a2324e2478db23d3b1c7169830c957b03
Niraj Dewangan <niraj.dewangan1608@gmail.com>
2022-03-10 16:06:33 +0530

    plotLabelCountGraph api code

--- controllers/plotLabelCountGraph.js
+++ controllers/plotLabelCountGraph.js
@@ -21,7 +21,7 @@ router.get('/', ((req, res, next) => {
 	// }
     let query;
     if(!model){
-        query = `SELECT DISTINCT * FROM main`;
+        query = `SELECT model, true_class, capture_date, COUNT(*) as capture_time FROM main WHERE capture_date >= DATE_ADD(CURDATE(), INTERVAL -2 DAY) GROUP BY true_class, capture_date`;
     }else{
         query = `SELECT DISTINCT * FROM main WHERE model="${model}"`;
 		if(st_time && end_time ){
@@ -33,6 +33,10 @@ router.get('/', ((req, res, next) => {
 	console.log(query);
 
 	pool.promise().query(query).then(([rows,fields])=>{
+		rows.map( (item) => {
+			item.capture_date = new Date(item.capture_date).getDate();
+		})
+		
 		res.send(JSON.stringify({"status": 200, "error": null, "response": rows}));
     })
 	.catch((err)=>{
