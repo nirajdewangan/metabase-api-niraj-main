e3c91107788d4d1c1f93898675c4e87777ea7439
Pankaj Kumar Agarwal <pankaj.7613@gmail.com>
2022-03-24 10:50:55 +0530

    config chnage for prod

--- client/src/app.config.js
+++ client/src/app.config.js
@@ -1,5 +1,5 @@
 //GlobalUri for api calls
-export const globalUri = "http://localhost:3000/api/";
-export const globalUri2 = "http://localhost:3000";
+export const globalUri = "/api/";
+export const globalUri2 = "";
 
 
